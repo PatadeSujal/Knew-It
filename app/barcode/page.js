9afe4c68a297641bd937ef3251c5c59045ffbadc
PatadeSujal <patadesujal@gmail.com>
2025-07-27 21:20:53 +0530

    beta tag added

--- app/barcode/page.js
+++ app/barcode/page.js
@@ -1,11 +1,19 @@
 "use client";
 import React from 'react';
 import BarcodeReader from '../components/BarcodeReader';
-
+import { FaCircle } from "react-icons/fa";
 const Page = () => {
   return (
     <>
       {/* Uncomment to enable barcode scanning */}
+      <div className="flex items-center gap-2 m-5">
+  <span className="flex items-center gap-1 bg-yellow-100 text-yellow-800 text-xs font-medium px-2.5 py-0.5 rounded-sm dark:bg-yellow-900 dark:text-yellow-300">
+    <FaCircle className="text-amber-700 text-[8px]" />
+    Beta
+  </span>
+</div>
+
+
       <BarcodeReader />
 
       {/* <div className="coming-soon-container mx-auto flex justify-center items-center flex-col gap-4 my-10">
